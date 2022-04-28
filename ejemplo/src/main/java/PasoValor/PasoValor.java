08d04b8d43c7625bba8d3d4b5e18a23f4d95b6bd
stanleeeys <69662538+stanleeeys@users.noreply.github.com>
2022-04-27 21:33:41 -0600

    Add files via upload

---
+++ ejemplo/src/main/java/PasoValor/PasoValor.java
@@ -0,0 +1,18 @@
+
+package PasoValor;
+
+public class PasoValor {
+    public static void main(String[] args) {
+        var guardar = 10;
+        System.out.println("guardar="+ guardar);
+        
+        cambioValor(guardar);
+                
+        
+        
+    }
+    public static void cambioValor(int arg1) {
+        System.out.println("arg1= "+arg1);
+    }
+    
+}
